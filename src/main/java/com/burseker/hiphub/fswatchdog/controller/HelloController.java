b813a05b13516b890fb9fa6b7d8f1c6df3dab14c
SVB <spybreakp-2@yandex.ru>
2023-02-27 10:02:25 +0300

    Encoding issue in work

--- src/main/java/com/burseker/hiphub/fswatchdog/controller/HelloController.java
+++ src/main/java/com/burseker/hiphub/fswatchdog/controller/HelloController.java
@@ -8,6 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.List;
 
@@ -23,6 +28,22 @@ public class HelloController {
         log.info("getVersion entry");
         log.debug("debug log string");
         return "hiphub version 0.0.1-SNAPSHOT";
+    }
+
+    @GetMapping("test-endpoint")
+    String testEndpoint() throws UnsupportedEncodingException {
+        log.info("testEndpoint(). entry");
+
+        String testString = "Какие то символы на русском";
+        log.info(testString);
+        log.info(new BigInteger(1, testString.getBytes()).toString(16));
+        String utf8EncodedString = new String(testString.getBytes(StandardCharsets.UTF_8));
+        log.info(utf8EncodedString);
+        log.info(new BigInteger(1, utf8EncodedString.getBytes()).toString(16));
+
+        log.info("Кодировка по умолчанию {}", Charset.defaultCharset());
+        //https://www.geeksforgeeks.org/how-to-get-and-set-default-character-encoding-or-charset-in-java/
+        return testString;
     }
 
     @GetMapping("walk-for-copies")
