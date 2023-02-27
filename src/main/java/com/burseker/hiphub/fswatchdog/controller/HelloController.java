ef1af619cceaa739bdbb9f06240949dd2b706e3f
burseker <spybreakp-2@yandex.ru>
2023-02-27 18:59:12 +0300

    some fixes for default code page

--- src/main/java/com/burseker/hiphub/fswatchdog/controller/HelloController.java
+++ src/main/java/com/burseker/hiphub/fswatchdog/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.burseker.hiphub.fswatchdog.controller;
 
-import com.burseker.hiphub.fswatchdog.file_indexer.FileMetaInfo;
 import com.burseker.hiphub.fswatchdog.service.FileService;
 import com.burseker.hiphub.fswatchdog.view.FileWithCopies;
 import lombok.extern.slf4j.Slf4j;
@@ -8,9 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.UnsupportedEncodingException;
+import java.io.*;
+
 import java.math.BigInteger;
-import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Collection;
@@ -41,10 +40,44 @@ public class HelloController {
         log.info(utf8EncodedString);
         log.info(new BigInteger(1, utf8EncodedString.getBytes()).toString(16));
 
-        log.info("Кодировка по умолчанию {}", Charset.defaultCharset());
+        // Setting the file encoding explicitly
+        // to a new value
+        //System.setProperty("file.encoding", "UTF-8");
+
+        //рабочий метод для IDEA, не очень хороший способ, надо еще копать
+        //https://youtrack.jetbrains.com/issue/IDEA-276155/Unable-to-change-gradle-build-output-encoding
+
+        log.info("Кодировка по умолчанию Charset.defaultCharset(): {}", Charset.defaultCharset());
+        log.info("Кодировка по умолчанию System.getProperty(\"file.encoding\"){}", System.getProperty("file.encoding"));
+        log.info("Кодировка по умолчанию getCharacterEncoding(){}", getCharacterEncoding());
         //https://www.geeksforgeeks.org/how-to-get-and-set-default-character-encoding-or-charset-in-java/
         return testString;
     }
+
+    public static String getCharacterEncoding()
+    {
+
+        // Creating and initializing byte array
+        // with some random character say it be N
+
+        // Here N = w
+        byte[] byte_array = { 'w' };
+
+        // Creating an object of inputStream
+        InputStream instream
+                = new ByteArrayInputStream(byte_array);
+
+        // Now, opening new file input stream reader
+        InputStreamReader streamreader
+                = new InputStreamReader(instream);
+
+        String defaultCharset = streamreader.getEncoding();
+
+        // Returning the default character encoded
+        // Here it is for N = 'w'
+        return defaultCharset;
+    }
+
 
     @GetMapping("walk-for-copies")
     String walForCopies(){
